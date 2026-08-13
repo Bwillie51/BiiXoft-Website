fa9eb1c8708cf8c8f44ed1f04e07cfbe47db7832
Brian WILLIE <brianwillie17@gmail.com>
2026-08-13 12:05:41 +1000

    fix: corrected runtime project id value for sanity client

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client'
 
 export const sanityClient = createClient({
   // Go to your biixoft-studio/sanity.config.js file to copy your exact 8-character projectId string
-  projectId: 'your_actual_sanity_project_id_here', 
+  projectId: 'p7hok33v', 
   dataset: 'production',
   useCdn: false, // Set to false to ensure immediate real-time data updates on publish!
   apiVersion: '2026-08-13', // Uses the current calendar operational date
